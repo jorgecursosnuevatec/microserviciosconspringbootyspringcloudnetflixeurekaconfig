6b9c979ea54319da1072b94655832b9dbb62ba0d
unknown <jorgecursosnuevatec@gmail.com>
2023-02-07 16:19:17 +0100

    Tue Feb  7 16:19:17     20232023-02-07 properties entornos para microservicio-items

--- gitbasico.sh
+++ gitbasico.sh
@@ -20,7 +20,7 @@ inicio=`date +%s`
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2023-02-06 FUNCIONA OK"
+message="$(date)2023-02-07 properties entornos para microservicio-items"
 fi
 
 git add .
